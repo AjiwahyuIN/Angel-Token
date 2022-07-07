7856f84b88cecfc30e7e10e4129e5dd535918e4e
ajiwahyu <ajiewahyu24@gmail.com>
2022-07-08 02:41:17 +0700

    market  mobile

--- _site/assets/js/index.js
+++ _site/assets/js/index.js
@@ -151,4 +151,18 @@ $('.btn-sidenav-m').click(function(){
 
 
 
-
+new TradingView.widget(
+  {
+  "autosize": true,
+  "symbol": "NASDAQ:AAPL",
+  "interval": "D",
+  "timezone": "Etc/UTC",
+  "theme": "light",
+  "style": "1",
+  "locale": "en",
+  "toolbar_bg": "#f1f3f6",
+  "enable_publishing": false,
+  "allow_symbol_change": true,
+  "container_id": "tradingview_f8b38"
+}
+  );
